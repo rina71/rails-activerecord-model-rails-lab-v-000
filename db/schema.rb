04f69d6723ffb488a6c26c1d6917ad12dadace6b
Yurina Shin <yurinas@gmail.com>
2018-10-24 23:37:13 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20181024233550) do
+
+  create_table "students", force: :cascade do |t|
+  end
 
 end
